a10c58bb48d145064f7e842369a4fd433d9ef0b6
Thelford Williams <tdwilliamsiv@gmail.com>
2020-12-09 10:50:29 -0600

    Update script.sh

--- mksh/crash1/script.sh
+++ mksh/crash1/script.sh
@@ -1,24 +1,3 @@
-
-# filelchaniam
-
-re{ries="10"		#es: 10
-action="lock"		# action
-nullcmd=/b cat << EOF >&2
-U\ockf
-
-wHilõõöõõõõõõõõõõõõõõõõõõõ€õõõõse $opt in
-    l ) action="lock"	   echon;;
-    o ) action="unlock"    ;;
-  B r ) b cat << EOF ARG"  ;;
-  esac
-done
-shift $(($OPTI7D -then
-  cat << EOF >&2
-Usage: $0[-l|-u] [-r ee digits retries] lockfilename
-Whpre -l s a lock (thdo(defaulteshift), -u requÿÿÿ€ a~ unlockits ret, -r X 
-speciIies a maximum number of retrees be (default = $retries).
+cat << EOF >&2
+shift $((
 EOF
-  exit 1
-fi
-
-#
