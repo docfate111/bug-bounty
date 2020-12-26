83eecefcd5eefd859b07a6c9b764d0f8ef8fc97b
Thelford Williams <tdwilliamsiv@gmail.com>
2020-12-26 16:50:45 -0600

    Update setup.sh

--- mupdf/setup.sh
+++ mupdf/setup.sh
@@ -3,5 +3,6 @@ git clone https://github.com/ArtifexSoftware/mupdf.git
 cd mupdf
 git submodule update --init
 sudo CC="hfuzz-clang" CXX="hfuzz-clang++"  make HAVE_X11=no HAVE_GLUT=no prefix=/usr/local install
-mv ../harness.c .
+cp ../harness.c harness.c
 hfuzz-clang -I/usr/local/include -o harness harness.c /usr/local/lib/libmupdf.a /usr/local/lib/libmupdf-third.a -lm
+honggfuzz -i ~/seeds --crashdir output -- ./harness ___FILE___ 0
