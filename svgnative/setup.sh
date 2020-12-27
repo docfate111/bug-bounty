8626f00550c08b3c09846b2586ea123cdcd5f039
Thelford Williams <tdwilliamsiv@gmail.com>
2020-12-26 19:02:40 -0600

    Create setup.sh

---
+++ svgnative/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/adobe/svg-native-viewer.git
+cd svg-native-viewer
+git submodule update --init
+sudo apt-get install build-essential libboost-system-dev
+cd svg-native
+cmake -Bbuild/linux -H. -DCMAKE_CXX_COMPILER=hfuzz-clang++
+cp include/svgnative/ports/string/StringSVGRenderer.h ports/string/StringSVGRenderer.h
+cd build/linux
+sudo make install
+cd ../..
+honggfuzz -n 2 -i seeds --crashdir output build/linux/example/testC/testC ___FILE___ output
